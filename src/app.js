bb3e9f05a543feb52912cae3af2878f25281820c
Lian Katherin Guevara <liann.guevara@gmail.com>
2022-11-04 20:24:36 -0500

    update contact and footer

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const menu_hamburguer = document.querySelector(".menu");
+
+const button_hamburguer = document.querySelector(".hamburguer")
+
+
+button_hamburguer.addEventListener("click", WatchHiden);
+
+menu_hamburguer.classList.add("menu_active");
+
+function WatchHiden() {
+    if (menu_hamburguer.classList.contains("menu_active")) {
+        menu_hamburguer.classList.remove("menu_active")
+    }
+    else {
+        menu_hamburguer.classList.add("menu_active")
+    }
+}
